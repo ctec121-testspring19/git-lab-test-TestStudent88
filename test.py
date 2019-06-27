d6dd28b0fa6bb63fa87144b63bec13ccb6477067
Bill Baker <bill@baker88.com>
2019-06-27 16:31:01 -0700

    final

--- test.py
+++ test.py
@@ -3,8 +3,10 @@
 # TestStudent88
 
 def main():
+    print()
     # lab: inserted text in original message
     print("Hello there World!")
     print("print added during lab")
+    print()
 
 main()
